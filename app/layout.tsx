7163cc8dcaf749b703ffaff911f728bce062acbc
Udit19199 <udit19199@gmail.com>
2025-11-26 07:08:33 +0530

    Init

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,14 +14,14 @@ export const viewport: Viewport = {
 export const metadata: Metadata = {
   metadataBase: new URL('https://nim-fawn.vercel.app/'),
   alternates: {
-    canonical: '/'
+    canonical: '/',
   },
   title: {
-    default: 'Nim - Personal website template',
-    template: '%s | Nim'
+    default: 'Udit Agarwal',
+    template: '%s | Udit Agarwal',
   },
-  description:  'Nim is a free and open-source personal website template built with Next.js 15, React 19 and Motion-Primitives.',
-};
+  description: 'Personal website and portfolio of Udit Agarwal.',
+}
 
 const geist = Geist({
   variable: '--font-geist',
